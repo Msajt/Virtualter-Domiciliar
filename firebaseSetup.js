8e01ca72edc8a3086925adb8049b5d78a74d4a9a
Paulo César <47919954+Msajt@users.noreply.github.com>
2022-01-08 14:42:13 -0300

    update pathnames

--- firebaseSetup.js
+++ firebaseSetup.js
@@ -80,7 +80,7 @@ const InsertData = (userId, userEmail, userType) => {
             //? Limpando os dados
             document.getElementById('register-email').value = '';
             document.getElementById('register-password').value = '';
-            window.location = '/Virtualter-Domiciliar/index.html';
+            window.location = './index.html';
         })
         .catch((error)=> {
             const errorCode = error.code;
@@ -95,7 +95,7 @@ const InsertData = (userId, userEmail, userType) => {
 }
 
     //? Verificando se a página é a 'register.html' (registro)
-    if(window.location.pathname == '/Virtualter-Domiciliar/register.html') document.getElementById('register-button').addEventListener ("click", Register);
+    if(window.location.pathname == './register.html') document.getElementById('register-button').addEventListener ("click", Register);
 
 //! =================================================================================
 //! ||                             LOGIN DO USUÁRIO                                ||
@@ -110,7 +110,7 @@ const Login = () => {
     signInWithEmailAndPassword(auth, userEmail, userPassword)
         .then((userCredential) => {
             //TODO Encaminhar para a página
-            window.location = '/Virtualter-Domiciliar/gamePage/index.html';
+            window.location = './gamePage/index.html';
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -121,6 +121,6 @@ const Login = () => {
 }
 
     //? Verificando se a página é a 'index.html' (login)
-    if(window.location.pathname == '/Virtualter-Domiciliar/index.html' || window.location.pathname == '/Virtualter-Domiciliar/') document.getElementById('login-button').addEventListener ("click", Login);
+    if(window.location.pathname == './index.html' || window.location.pathname == './') document.getElementById('login-button').addEventListener ("click", Login);
 
 
